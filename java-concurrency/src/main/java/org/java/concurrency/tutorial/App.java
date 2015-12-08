5d1031273c9997d0f9f31cfcb7e61a837e0a395e
dtbwije <dtbwije@gmail.com>
2015-12-08 21:39:07 +0200

    Commit first labda thread

--- java-concurrency/src/main/java/org/java/concurrency/tutorial/App.java
+++ java-concurrency/src/main/java/org/java/concurrency/tutorial/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        FirstRunnableClass firstClass = new FirstRunnableClass();
+        firstClass.run();
         System.out.println( "Hello World!" );
     }
 }
--- java-concurrency/src/main/java/org/java/concurrency/tutorial/FirstRunnableClass.java
+++ java-concurrency/src/main/java/org/java/concurrency/tutorial/FirstRunnableClass.java
@@ -4,4 +4,17 @@ package org.java.concurrency.tutorial;
  * Created by tharanga on 12/6/15.
  */
 public class FirstRunnableClass {
+
+    public void run(){
+        Runnable runnable = () -> {
+            String threadname = Thread.currentThread().getName();
+            System.out.println("Hello " + threadname);
+        };
+        runnable.run();
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        System.out.println("Done !");
+    }
 }
